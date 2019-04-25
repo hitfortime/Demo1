1ce08b2ec2c07ef14dace0118bbdc867721e3ee1
hitfortime <49878242+hitfortime@users.noreply.github.com>
2019-04-25 14:16:10 +0300

    sdhfh

---
+++ DemoApp/DemoApp/Auth.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DemoApp
+{
+    public partial class Auth : Form
+    {
+        Form reg;
+        public Auth()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            this.Hide();
+            reg = new Reg();
+            reg.Show();
+        }
+    }
+}
